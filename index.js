02f52cb046bc3d2fdbf30277fed2566879690dd0
= <mukherjee4004@gmail.com>
2023-03-21 10:28:45 +0530

    add again

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.post("/api/wh",(req,res)=>{
-    console.log(req.body)
+    console.log(req.body._id)
     res.status(200).json({id : req.body});
 })
 
