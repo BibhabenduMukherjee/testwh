9eac0705bcd45d51ca27986d6369d4ade6a7456a
= <mukherjee4004@gmail.com>
2023-03-21 10:25:57 +0530

    add express json parser

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express")
 const app = express();
 const cors = require("cors")
 app.use(cors());
-
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 app.post("/api/wh",(req,res)=>{
     console.log(req.body)
     res.status(200).json({id : req.body});
